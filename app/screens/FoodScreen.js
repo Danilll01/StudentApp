1bc33a53858a2ac52a2e016156739daee9cbbb5a
Daniel <perssondaniel01@gmail.com>
2022-07-10 14:57:15 +0200

    Added a searchbar

--- app/screens/FoodScreen.js
+++ app/screens/FoodScreen.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React, {useState} from 'react';
+import { Platform, SafeAreaView } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 import styles from './ScreenStyle.js';
 
+// UI library 
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text, Card, Button } from '@ui-kitten/components';
+
 function FoodScreen(props) {
+    const [theme, setTheme] = useState(eva.light);
+    const [search, setSearch] = useState("");
+
+    const updateSearch = (search) => {
+        setSearch(search);
+    };
+
     return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.headerText}>Vad vill du äta idag?</Text>
-        </SafeAreaView>
+        <ApplicationProvider {...eva} theme={theme}>
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.headerText} category='h1'>Vad vill du äta idag?</Text>
+                <SearchBar 
+                    platform={Platform.OS}
+                    placeholder="Type Here..."
+                    onChangeText={updateSearch}
+                    value={search}
+                    onCancel={() => setSearch("")}
+                    />
+            </SafeAreaView>
+        </ApplicationProvider>
     );
 }
+
+
 
 export default FoodScreen;
