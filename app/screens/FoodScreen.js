524ef11e6e7af57e16d4fd25b029a87b6b50410f
Daniel <perssondaniel01@gmail.com>
2022-07-10 22:51:24 +0200

    Added radio button functionallity to category tags

--- app/screens/FoodScreen.js
+++ app/screens/FoodScreen.js
@@ -3,6 +3,8 @@ import { Platform, SafeAreaView } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import styles from './ScreenStyle.js';
 
+import Tag from '../components/Tag';
+
 // UI library 
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text, Card, Button } from '@ui-kitten/components';
@@ -10,10 +12,21 @@ import { ApplicationProvider, Layout, Text, Card, Button } from '@ui-kitten/comp
 function FoodScreen(props) {
     const [theme, setTheme] = useState(eva.light);
     const [search, setSearch] = useState("");
+    const [tags, setTags] = useState([{id: 0, active: false}, {id: 1, active: false}, {id: 2, active: false}]);
 
     const updateSearch = (search) => {
         setSearch(search);
     };
+
+    const setActiveTag = (id) => {
+        let updatedState = (tags.map(tag => (
+                tag.id === id
+                ? { ...tag, active: !tag.active }
+                : { ...tag, active: false }
+        )));
+
+        setTags(updatedState);
+    }
 
     return (
         <ApplicationProvider {...eva} theme={theme}>
@@ -25,7 +38,15 @@ function FoodScreen(props) {
                     onChangeText={updateSearch}
                     value={search}
                     onCancel={() => setSearch("")}
-                    />
+                />
+                <Layout style={{flexDirection: 'row', paddingLeft: 10}}>
+                    {tags.map(tag => {
+                        console.log(tag.id + " " + tag.active)
+                        return (
+                            <Tag key={tag.id} active={tag.active} onPress={() => setActiveTag(tag.id)} />
+                        );
+                    })}
+                </Layout>
             </SafeAreaView>
         </ApplicationProvider>
     );
