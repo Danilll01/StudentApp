08b1d74924abec33d5c9c3fb394c1a9c871f57c7
Daniel <perssondaniel01@gmail.com>
2022-07-11 22:24:39 +0200

    Added RecipeItem and temporary recipe data

--- app/screens/FoodScreen.js
+++ app/screens/FoodScreen.js
@@ -1,13 +1,37 @@
 import React, {useState} from 'react';
-import { Platform, SafeAreaView } from 'react-native';
+import { Platform, SafeAreaView , ScrollView } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import styles from './ScreenStyle.js';
 
 import Tag from '../components/Tag';
+import RecipeItem from '../components/RecipeItem';
 
 // UI library 
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text, Card, Button } from '@ui-kitten/components';
+
+
+const recipeData = [
+    {
+        title: "Lättlagad lax i ugn",
+        cookTime: 40,
+        image: "image",
+        tags: ["fish"], //"meat", "chicken"
+        servings: 2,
+        ingredients: [
+            {
+                amount: 400,
+                unit: "g",
+                name: "Laxfile"
+            },
+            {
+                amount: 1,
+                unit: "st",
+                name: "Citron"
+            }
+        ],
+    }
+]
 
 function FoodScreen(props) {
     const [theme, setTheme] = useState(eva.light);
@@ -34,19 +58,25 @@ function FoodScreen(props) {
                 <Text style={styles.headerText} category='h1'>Vad vill du äta idag?</Text>
                 <SearchBar 
                     platform={Platform.OS}
-                    placeholder="Type Here..."
+                    placeholder="Sök efter ett recept"
                     onChangeText={updateSearch}
                     value={search}
                     onCancel={() => setSearch("")}
                 />
                 <Layout style={{flexDirection: 'row', paddingLeft: 10}}>
-                    {tags.map(tag => {
-                        console.log(tag.id + " " + tag.active)
-                        return (
-                            <Tag key={tag.id} active={tag.active} onPress={() => setActiveTag(tag.id)} />
-                        );
-                    })}
+                    {tags.map(tag => (
+                            <Tag key={tag.id} id={tag.id} active={tag.active} onPress={() => setActiveTag(tag.id)} />
+                        )
+                    )}
                 </Layout>
+                <ScrollView style={{padding: 20, }}>
+                    {recipeData.map(recipe => (
+                            <RecipeItem key={recipe.title} recipe={recipe} />
+                        )
+                    )}
+                    <RecipeItem/>
+                    
+                </ScrollView>
             </SafeAreaView>
         </ApplicationProvider>
     );
