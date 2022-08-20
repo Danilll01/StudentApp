ec329910fafb357e65efe3971879db23ad79621e
Daniel <perssondaniel01@gmail.com>
2022-08-20 16:03:30 +0200

    Enabled persistant storage for recipes

--- app/redux/store.js
+++ app/redux/store.js
@@ -11,7 +11,7 @@ const persistConfig = {
 	key: "root",
 	version: 1,
 	storage: AsyncStorage,
-    whitelist: ['theme']
+    whitelist: ['theme', 'recipes']
 }
 const rootReducer = combineReducers({
 	theme: themeReducer,
